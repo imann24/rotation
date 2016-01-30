a645dbbc7a63f7d86477ad2df021cca4e1c2df85
Jowell-E <jjoelesquilin@gmail.com>
2016-01-30 12:37:53 -0500

    Level and Player Movement Demo
    We got a rotate thing and a moving thing

---
+++ Assets/2D test/PlayerMovement2D.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement2D : MonoBehaviour {
+	public float moveSpeed = 2f;
+	public float jumpForce = 200f;
+	private Rigidbody2D rb2d;
+	public bool frozen = false;
+
+	public LayerMask whatIsGround;  
+	private float groundedRadius = .2f; 
+	public bool grounded; 
+	private Transform groundCheck;
+	private bool facingRight = false;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		groundCheck = transform.Find("GroundCheck");
+	}
+
+
+	void FixedUpdate(){
+		if (frozen) {
+			rb2d.constraints = RigidbodyConstraints2D.FreezePosition;
+			//return here to not flip
+		} else {
+			rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+		}
+
+		float h = Input.GetAxis ("Horizontal");
+		bool jump = Input.GetKeyDown (KeyCode.Space);
+
+		if (Physics2D.OverlapCircle (groundCheck.position, groundedRadius, whatIsGround)) {
+			grounded = true;
+		} else {
+			grounded = false;
+		}
+
+		if (h < 0 && facingRight) {
+			Flip ();
+		} else if (h > 0 && !facingRight) {
+			Flip ();
+		}
+		// if grounded can't move in air
+		rb2d.velocity = new Vector2(moveSpeed * h, rb2d.velocity.y);
+		if (grounded && jump)
+		{
+			// Add a vertical force to the player.
+			grounded = false;
+			jump = false;
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale; 
+	}
+
+	public void DisableColliders(bool state){
+		Transform[] children = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++){
+			children[i] = transform.GetChild(i);
+		}
+		foreach(Transform child in children){
+			if (child.name != "GroundCheck") {
+				child.gameObject.GetComponent<BoxCollider2D> ().enabled = state;
+			}
+		}
+	}
+}
---
+++ Assets/2D test/RotateThingsScript.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateThingsScript : MonoBehaviour {
+	public GameObject level;
+	public GameObject player;
+
+	public float rotateSpeed = 90f;
+	private PlayerMovement2D p_movement;
+
+	private bool rotating = false;
+	public bool frozen = false;
+	// Use this for initialization
+	void Start () {
+		p_movement = player.GetComponent<PlayerMovement2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		
+		if (Input.GetKeyDown(KeyCode.F)){
+			if (!rotating) {
+				if (!frozen) {
+					if (p_movement.grounded) {
+						p_movement.frozen = true;
+						p_movement.DisableColliders (false);
+					} else {
+						return;
+					}
+				}
+				if (frozen){
+					p_movement.frozen = false;
+					p_movement.DisableColliders (true);
+				}
+				frozen = !frozen;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			if (frozen) {
+				if (!rotating) {
+					StartCoroutine ("RotateCounterClockwise", level);
+				}
+			}
+		}if (Input.GetKeyDown (KeyCode.E)) {
+			if (frozen) {
+				if (!rotating) {
+					StartCoroutine ("RotateClockwise", level);
+				}
+			}
+		}
+
+	}
+	IEnumerator RotateClockwise(GameObject target){
+		if(target == null){
+			Debug.LogError("target game object is null");
+			yield return null;
+		}
+		rotating = true;
+
+		float endRotation = Mathf.Repeat(target.transform.localEulerAngles.z - rotateSpeed, 360f);
+
+		float rotationDoneSoFar = 0.0f;
+
+		while (rotationDoneSoFar <= 90.0f){
+			float rotateBy = Time.deltaTime * 90.0f;
+			target.transform.Rotate(Vector3.forward, -rotateBy);
+			rotationDoneSoFar += rotateBy;
+			yield return null;
+		}
+		target.transform.rotation = Quaternion.Euler(new Vector3(0, 0, endRotation));
+		rotating = false;
+	}
+
+		
+	IEnumerator RotateCounterClockwise(GameObject target){
+		if (target == null) {
+			Debug.LogError ("target game object is null");
+			yield return null;
+		}
+		rotating = true;
+
+		float endRotation = Mathf.Repeat (target.transform.localEulerAngles.z + rotateSpeed, 360f);
+
+		float rotationDoneSoFar = 0.0f;
+
+		while (rotationDoneSoFar <= 90.0f) {
+			float rotateBy = Time.deltaTime * 90.0f;
+			target.transform.Rotate (Vector3.forward, rotateBy);
+			rotationDoneSoFar += rotateBy;
+			yield return null;
+		}
+
+		target.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, endRotation));
+		rotating = false;
+	}
+}
