a37067f395e46ec638f74fcc4b6d6c286f670e9c
Timothy Ng <tdng@umass.edu>
2016-01-30 14:44:23 -0500

    Spikes are implemented

---
+++ Assets/KillPlayerOnTouch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayerOnTouch : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Destroy(coll.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLevel : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TODO: Finish the level");
+    }
+}
