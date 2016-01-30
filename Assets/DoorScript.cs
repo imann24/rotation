ab4bee5ddf62681b9879fa4202dadc9c8c553897
Timothy Ng <tdng@umass.edu>
2016-01-30 15:16:57 -0500

    Doors and Switches added

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+    public bool isDoorOpen;
+
+    void Start()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = !isDoorOpen;
+        gameObject.GetComponent<SpriteRenderer>().enabled = !isDoorOpen;
+    }
+
+	public void flipOpenClose()
+    {
+        if (isDoorOpen)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        isDoorOpen = !isDoorOpen;
+    }
+}
---
+++ Assets/SwitchScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour {
+
+    public DoorScript[] doors;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            foreach (DoorScript d in doors)
+            {
+                if(d == null)
+                {
+                    Debug.Log("There is a null door that this switch is operating");
+                }
+                d.flipOpenClose();
+            }
+        }
+
+    }
+}
