860e2dc142ab6c168ded813d1a2f93d3c44c1199
imann24 <imann24@gmail.com>
2016-01-30 18:56:26 -0500

    Pushing main menu

--- Assets/Scripts/MainMenuScripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuScripts/MainMenuController.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenuController : MonoBehaviour {
+	public Text LevelName;
+
+	int _levelToLoad;
+
+	string [] _levelNames = {
+		"Main Menu",
+		"Level 1",
+		"Level 2",
+		"Level 3",
+		"Level 4",
+		"Level 5",
+		"Level 6"
+	};
 
 	// Use this for initialization
 	void Start () {
 		EventController.Event("menuMusic");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void SetLevel (int levelNumber) {
+		setLevelName(_levelNames[levelNumber]);
+		_levelToLoad = levelNumber;
+	}
+
+	public void LoadLevel () {
+		LoadLevel(_levelToLoad);
+	}
+
+	public void LoadLevel (int levelNumber) {
+		SceneManager.LoadScene(levelNumber);
+	}
+
+	void setLevelName (string levelName) {
+		LevelName.text = levelName;
 	}
 }
