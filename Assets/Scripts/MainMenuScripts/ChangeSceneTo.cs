dfc9cad3298e83d3c8e76df114859c306e4db977
Timothy Ng <tdng@umass.edu>
2016-01-29 20:26:03 -0500

    Added MainMenu Functionality start button

---
+++ Assets/Scripts/MainMenuScripts/ChangeSceneTo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneTo : MonoBehaviour {
+	
+	public void ChangeToScene (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
