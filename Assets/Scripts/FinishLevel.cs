2da41fd521b5c274f20d32ea1df73c332cdf9e35
imann24 <imann24@gmail.com>
2016-01-30 20:05:41 -0500

    Added next level loading

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -6,5 +6,6 @@ public class FinishLevel : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("TODO: Finish the level");
+		SceneUtil.LoadNextScene();
     }
 }
--- Assets/Scripts/Util/SceneUtil.cs
+++ Assets/Scripts/Util/SceneUtil.cs
@@ -3,8 +3,24 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public static class SceneUtil {
-
+	const int _mainMenu = 0;
 	public static bool IsMainMenu () {
-		return SceneManager.GetActiveScene().buildIndex == 0;
+		return SceneManager.GetActiveScene().buildIndex == _mainMenu;
+	}
+
+	public static void LoadNextScene () {
+		if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCount) {
+
+			SceneManager.LoadScene (
+				_mainMenu
+			);
+
+		} else {
+
+			SceneManager.LoadScene (
+				SceneManager.GetActiveScene().buildIndex + 1
+			);
+
+		}
 	}
 }
